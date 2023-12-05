47f4756382309b06f9c13ab06ddb540789365190
Chris Purcell <chrisp0000@gmail.com>
2023-12-04 18:31:39 -0800

    Added route for notes.html in server.js

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+// app.get()
 
 app.get('*', (req, res) =>
     res.send(
