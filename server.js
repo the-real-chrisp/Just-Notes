11837feaf7580170dbe1f92424c9c3fcd0396f6c
Chris Purcell <chrisp0000@gmail.com>
2023-12-04 16:50:24 -0800

    Added in basic routes to root, notes, and fall back route. Input middleware to parse JSON and urlencoded data.

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const app = express ();
+const app = express();
 
 const PORT = process.env.PORT || 3001
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.get('*', (req, res) =>
+    res.send(
+        `Make a GET request using Insomnia to <a href="http://localhost:${PORT}/api/terms">http://localhost:${PORT}/api/terms</a>`
+    )
+)
+
+app.listen(PORT, () =>
+    console.info(`App listening at http://localhost:${PORT}`)
+);
