af066944c9d3efe8417abcc664bd98542daa4e84
Chris Purcell <chrisp0000@gmail.com>
2023-12-07 16:21:55 -0800

    added uuid. updated routes for html and api in server.js. updated api and html routers

--- routes/html.js
+++ routes/html.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.use('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
-router.use('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+router.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const path = require('path');
 const html = require('./routes/html')
 const api = require('./routes/api');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/', html)
 app.use('/api', api);
 
 app.get('*', (req, res) =>
     res.send(
-        `Make a GET request using Insomnia to <a href="http://localhost:${PORT}/api/terms">http://localhost:${PORT}/api/terms</a>`
+        `Make a GET request using Insomnia to <a href="http://localhost:${PORT}">http://localhost:${PORT}</a>`
     )
 )
 
